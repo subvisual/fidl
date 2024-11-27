63e604b6a025fc286200732f0756e15946d1eee0
subriver <tiago.gomes@subvisual.group>
2024-11-27 15:10:06 +0000

    FIDL: types changes (#80)

--- types/address.go
+++ types/address.go
@@ -1,11 +1,22 @@
 package types
 
 import (
+	"fmt"
+
 	"github.com/filecoin-project/go-address"
 )
 
 type Address struct {
 	*address.Address
+}
+
+func NewAddressFromString(value string) (Address, error) {
+	addr, err := address.NewFromString(value)
+	if err != nil {
+		return Address{}, fmt.Errorf("failed to convert string to filecoin address: %w", err)
+	}
+
+	return Address{Address: &addr}, nil
 }
 
 func (a *Address) UnmarshalText(value []byte) error {
